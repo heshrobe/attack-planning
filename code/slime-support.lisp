141ba95c6caa3f9796d44cd17c97a86ef0d33d30
hes <hes@csail.mit.edu>
2021-10-18 14:11:09 -0400

    Updating all apps

--- code/slime-support.lisp
+++ code/slime-support.lisp
@@ -3,8 +3,19 @@
 (in-package :swank)
 
 
-#+mcclim
 (defmethod form-parser ((key (eql 'aplan::define-aplan-predicate)) form)
   (values (second form) :joshua-predicate))
 
-;;; still need support for defattack-method, define-action and define-aplan-object
+(defmethod form-parer ((key (eql 'aplan::defattack-method)) form)
+  (values (second form) :joshua-defrule))
+
+(defmethod form-parser ((key (eql 'aplan::define-action)) form)
+  (values (second form) :joshua-defrule))
+
+(defmethod form-parser ((key (eql 'aplan::define-aplan-object)) form)
+  (values (second form) :joshua-define-object-type))
+
+;;; Aplan has lots of defining forms for things in the environment
+;;; I may need to add more of these for all those things.
+;;; The thing is since they're all macros they're all pretty easy
+;;; like the ones above
